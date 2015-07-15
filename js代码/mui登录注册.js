17129ac14f9c08560fb928accc02357935bae1b9
s8186255 <suanqi@gmail.com>
2015-07-15 08:01:57 +0800

    增加部分mui代码

---
+++ js代码/mui登录注册.js
@@ -0,0 +1,124 @@
+/**
+ * 演示程序当前的 “注册/登录” 等操作，是基于 “本地存储” 完成的
+ * 当您要参考这个演示程序进行相关 app 的开发时，
+ * 请注意将相关方法调整成 “基于服务端Service” 的实现。
+ **/
+
+(function($, owner) {
+	/**
+	 * 用户登录
+	 **/
+	owner.login = function(loginInfo, callback) {
+		//获取参数，校验参数
+		callback = callback || $.noop;
+		loginInfo = loginInfo || {};
+		loginInfo.account = loginInfo.account || '';
+		loginInfo.password = loginInfo.password || '';
+		if (loginInfo.account.length < 5) {
+			return callback('账号最短为 5 个字符');
+		}
+		if (loginInfo.password.length < 6) {
+			return callback('密码最短为 6 个字符');
+		}
+		//获取本地用户信息，并与本地信息进行对照；
+		var users = JSON.parse(localStorage.getItem('$users') || '[]');
+		var authed = users.some(function(user) {
+			return loginInfo.account == user.account && loginInfo.password == user.password;
+		});
+		
+		if (authed) {
+			return owner.createState(loginInfo.account, callback);
+		} else {
+			return callback('用户名或密码错误');
+		}
+	};
+
+	owner.createState = function(name, callback) {
+		var state = owner.getState();
+		state.account = name;
+		state.token = "token123456789";
+		owner.setState(state);
+		return callback();
+	};
+
+	/**
+	 * 新用户注册
+	 **/
+	owner.reg = function(regInfo, callback) {
+		//获取数据并进行数据的校验；
+		callback = callback || $.noop;
+		regInfo = regInfo || {};
+		regInfo.account = regInfo.account || '';
+		regInfo.password = regInfo.password || '';
+		if (regInfo.account.length < 5) {
+			return callback('用户名最短需要 5 个字符');
+		}
+		if (regInfo.password.length < 6) {
+			return callback('密码最短需要 6 个字符');
+		}
+		if (!checkEmail(regInfo.email)) {
+			return callback('邮箱地址不合法');
+		}
+		//验证过程，先取本地存储中的用户信息，如果有，则取出，或者就是空；不管怎样users就是一个object；
+		var users = JSON.parse(localStorage.getItem('$users') || '[]');
+		//将用户填写的注册信息，写入到本地；
+		users.push(regInfo);
+		localStorage.setItem('$users', JSON.stringify(users));
+		//实际上还缺一个，应该将信息通过ajax的方式传递到用户服务端，实现真正的注册。
+		//现实中的逻辑应该是：将用户注册信息传到后台，后台返回正常后，在本地创建用户信息。
+		
+		//前期的会吐数据返回给调用者；
+		return callback();
+	};
+
+	/**
+	 * 获取当前状态
+	 **/
+	owner.getState = function() {
+		var stateText = localStorage.getItem('$state') || "{}";
+		return JSON.parse(stateText);
+	};
+
+	/**
+	 * 设置当前状态
+	 **/
+	owner.setState = function(state) {
+		state = state || {};
+		localStorage.setItem('$state', JSON.stringify(state));
+		//var settings = owner.getSettings();
+		//settings.gestures = '';
+		//owner.setSettings(settings);
+	};
+
+	var checkEmail = function(email) {
+		email = email || '';
+		return (email.length > 3 && email.indexOf('@') > -1);
+	};
+
+	/**
+	 * 找回密码
+	 **/
+	owner.forgetPassword = function(email, callback) {
+		callback = callback || $.noop;
+		if (!checkEmail(email)) {
+			return callback('邮箱地址不合法');
+		}
+		return callback(null, '新的随机密码已经发送到您的邮箱，请查收邮件。');
+	};
+
+	/**
+	 * 获取应用本地配置
+	 **/
+	owner.setSettings = function(settings) {
+		settings = settings || {};
+		localStorage.setItem('$settings', JSON.stringify(settings));
+	}
+
+	/**
+	 * 设置应用本地配置
+	 **/
+	owner.getSettings = function() {
+		var settingsText = localStorage.getItem('$settings') || "{}";
+		return JSON.parse(settingsText);
+	}
+}(mui, window.app = {}));
---
+++ js代码/mui窗口处理.js
@@ -0,0 +1,17 @@
+﻿//openWindow是一种方法，也是单独打开一个webview，注意要指定id，便于html+处理，避免打开过多的webview。
+mui.openWindow({
+	//id: tapId,
+	url: url,
+	styles: {
+		//top: '45px',
+		//bottom: '50px',
+		bounce: 'vertical',
+
+	},
+});
+
+//采用plus，处理起来更加灵活。
+hello=plus.webview.create('http://www.sina.com.cn','sina',{bottom:'45px'})
+hello.show();
+hello.hide();
+
